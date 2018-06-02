81ca0eb0c694e4c9af2c9fa3caeba74f63103407
krzysztofantecki <akrisu@gmail.com>
2018-06-03 01:26:29 +0200

    Add company map specs

--- src/app/lib/company/company.spec.ts
+++ src/app/lib/company/company.spec.ts
@@ -1,3 +1,46 @@
-it('works', () => {
-  expect(true).toBe(true);
+import { Company, ICompany } from './company';
+
+export const jsonCompany = {
+  name: 'Unit',
+  data: [
+    {
+      week: 50,
+      revenue: 10,
+      installations: 10,
+    }
+  ],
+};
+
+describe('company', () => {
+  it('should map single element into company', () => {
+    const json = jsonCompany;
+
+    const company = Company.mapOne(json);
+
+    const expectedCompany: ICompany = {
+      name: 'Unit',
+      statistics: [{
+        week: 50,
+        revenue: 10,
+        installations: 10,
+      }],
+    };
+
+    expect(company).toEqual(expectedCompany);
+  });
+
+  it('should map array of json elements into array of companies', () => {
+    const json = [jsonCompany];
+
+    const company = Company.mapArray(json);
+
+    const expectedCompanies: ICompany[] = [{
+        name: 'Unit',
+        statistics: [{
+          week: 50,
+          revenue: 10,
+          installations: 10,
+        }],
+    }];
+  });
 });
